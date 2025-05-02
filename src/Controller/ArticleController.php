6ed4a7a8b32f446e4c36319b9191ef922a3cf7f4
Yohann Dubuis <yohann.dubuis@lapiscine.pro>
2025-05-02 16:37:17 +0200

    recuperation post

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -3,13 +3,31 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ArticleController extends AbstractController{
 
     #[Route('/create-article', name: 'article')]
-    public function displayCreateArticle(){
-        return $this->render('create-article.html.twig', [
-        ]);
+    public function displayCreateArticle(Request $request){
+
+        if ($request->isMethod('POST')) {
+
+            $title = $request->request->get('title');
+            $description = $request->request->get('description');
+            $content = $request->request->get('content');
+            $image = $request->get('image');
+
+            dump($title);
+            dump($description);
+            dump($content);
+            dump($image);
+            ;die;
+
+        }
+           
+
+        return $this->render('create-article.html.twig', );
+
     }
 }

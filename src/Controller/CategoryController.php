8701b0fc9285c45383ace2f6451d3cd699461e27
Yohann Dubuis <yohann.dubuis@lapiscine.pro>
2025-05-06 14:25:51 +0200

    createForm

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -3,20 +3,23 @@
 namespace App\Controller;
 
 use App\Entity\Category;
+use App\Form\CategoryForm;
 use App\Repository\CategoryRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CategoryController extends AbstractController{
 
 
-#[Route('/category', name: 'category')]
+#[Route('/categories', name: 'categories')]
         
 public function displayCategory(CategoryRepository $categoryRepository){
 
     $category=$categoryRepository->findAll();
 
-        return $this->render('category.html.twig', [
+        return $this->render('list-categories.html.twig', [
             'categories' => $category,
         ]);
     }
@@ -33,6 +36,46 @@ public function displayCategory(CategoryRepository $categoryRepository){
         ]);
 
         
+    }
+
+    #[Route('/create-category', name: 'create-category')]
+    // on créé une route pour créer une catégorie
+
+    //je creer une methode pour afficher le formulaire de création de catégory
+    //en parmetre j'ouvre une instance pour request et une instance pour entity manager
+    public function displayCreateCategory(Request $request, EntityManagerInterface $entityManager){
+
+        // je cree une instance de la classe Category
+        $category = new Category();
+
+
+        // je creer un formulaire pour creer une category via la methode createForm
+        // je passe en parametre la classe CategoryForm et l'instance de la classe Category
+        $categoryForm =$this->createForm(CategoryForm::class,$category);
+
+
+        //handleRequest va récuperer les données du formulaire
+        //puis il va les assigner à l'instance de la classe Category
+        $categoryForm->handleRequest($request);
+
+
+        //je verifie si le formulaire à bien ete envoyé
+        if($categoryForm->isSubmitted()){
+
+            //persist va scanner les propriétés de l'entité category
+            //flush va envoyer la reequete a la base de donnée pour la mettre à jour.
+            $entityManager->persist($category);
+            $entityManager->flush();
+            
+        }
+        //je redirige vers la page de creation de category
+        return $this->render('create-category.html.twig', [
+			'categoryForm' => $categoryForm->createView()
+		]);
+
+
+
+
     }
 
 } 
---
+++ src/Form/CategoryForm.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Category;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+
+// cette class à ete genere par symfony lors de la creation du formulaire via le terminal 
+// php bin/console make:form 
+class CategoryForm extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title')
+            ->add('description')
+            ->add('color')
+            ->add('createdAt')
+            ->add('submit', SubmitType::class,)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Category::class,
+        ]);
+    }
+}
